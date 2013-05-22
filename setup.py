a16e3b5b5c22c5881eb33be5dd75a594414e8ddc
Hoa <hoameomu@gmail.com>
2013-05-22 12:54:51 +0800

    setup.py initial version

---
+++ setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Flask-Forward
+----------------
+
+Flask-Forward provides auto discovery, prioritization and rendering of template
+for Flask application.
+
+Links
+`````
+
+* `documentation <http://packages.python.org/Flask-Forward>`_
+* `development version
+  <http://github.com/saikocat/flask-forward>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Forward',
+    version='0.1.0',
+    url='https://github.com/saikocat/flask-forward',
+    license='MIT',
+    author='Nguyen Duc Hoa',
+    author_email='hoameomu@gmail.com',
+    description='Auto discovery, prioritization and rendering of template',
+    long_description=__doc__,
+    py_modules=['flask_forward'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'setuptools',
+        'Flask'
+    ],
+    test_suite='tests.forward.suite',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
